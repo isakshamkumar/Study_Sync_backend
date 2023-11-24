c671e66c83eafa8f24d4a12a7b7bb8aa9c5fe195
Saksham <ksaksham39@gmail.com>
2023-11-24 15:45:07 +0530

    final fixes

--- routes/Student.js
+++ routes/Student.js
@@ -1,6 +1,7 @@
 
 const express=require('express')
 const router=express.router()
+const authenticateJwtStudent=require('../middlewares/AuthStudent')
 
 
 
--- routes/Teacher.js
+++ routes/Teacher.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const router=express.router()
+const authenticateJwtTeacher=require('../middlewares/AuthTeacher')
 
 
 
